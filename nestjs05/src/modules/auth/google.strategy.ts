881be4f321a8d32655f40e2c4979a9f669c7b4f3
Hoang An Unicode <hoangan.web@gmail.com>
2025-03-27 09:27:30 +0700

    Update code

--- nestjs05/src/modules/auth/google.strategy.ts
+++ nestjs05/src/modules/auth/google.strategy.ts
@@ -7,11 +7,11 @@ import { AuthService } from './auth.service';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(private authService: AuthService) {
     super({
-      clientID:
-        '672062294064-0m77qi510hu4scqr4mqcg4bou024akb1.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:3000/auth/google/callback',
-      scope: ['email', 'profile'],
+      // clientID:
+      //   '672062294064-0m77qi510hu4scqr4mqcg4bou024akb1.apps.googleusercontent.com',
+      // clientSecret: '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',
+      // callbackURL: 'http://localhost:3000/auth/google/callback',
+      // scope: ['email', 'profile'],
     });
   }
 
